250daddc04cb3084b9c7023e8fe023840abe560d
Achintya Kumar Satyayan <67166146+aksatyayan-star@users.noreply.github.com>
2021-10-21 00:05:46 +0530

    Update third-maximum-number.java

--- third-maximum-number/third-maximum-number.java
+++ third-maximum-number/third-maximum-number.java
@@ -12,3 +12,13 @@ class Solution {
         return max;
     }
 }
+
+// Here the concept is pretty much simple
+// first you sort the array
+// then run a loop in the reverse direction so that we can start with the max number
+// so everytime when we will encounter an element different from the previous element 
+// we increase var i by one unit....i.e we increase the count
+// so as we need third largest , hence whenever our that i count equals three
+// it means we have encountered three elements till now which are distinct
+// and as we started from the reverse direction i.e from the largest element as sorted array
+// hence encountring 3rd distinct element means we have encountered 3rd largest element
