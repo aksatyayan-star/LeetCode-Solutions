2d3adf975457b465ac250fbda345a700a217197a
Achintya Kumar Satyayan <67166146+aksatyayan-star@users.noreply.github.com>
2021-10-25 23:56:02 +0530

    Time: 2 ms (59.68%), Space: 45.6 MB (16.97%) - LeetHub

---
+++ fizz-buzz/fizz-buzz.java
@@ -0,0 +1,24 @@
+class Solution {
+    public List<String> fizzBuzz(int n) {
+        List<String> lst=new ArrayList<>();
+        for(int i=1;i<=n;i++){
+            if(i%3 ==0 && i%5 == 0){
+                lst.add("FizzBuzz");
+            }
+            else if(i%3 ==0){
+                lst.add("Fizz");
+            }
+            else if(i%5 == 0){
+                lst.add("Buzz");
+            }
+            else{
+                lst.add(String.valueOf(i));
+            }
+        }
+        return lst;
+    }
+}
+
+// i guess it's a simple question and no explanation is needed for this
+// just we are iterating the loop till the given integer n and acc to conditions given in the question we are adding the elements in the list...that all 
+// and finally returning the list
