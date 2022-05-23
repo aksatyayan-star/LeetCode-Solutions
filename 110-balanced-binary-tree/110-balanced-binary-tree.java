4869e6d78a55564cb938a74c96c7b881b8479763
Achintya Kumar Satyayan <67166146+aksatyayan-star@users.noreply.github.com>
2022-05-23 16:09:21 +0530

    Time: 1 ms (94.82%), Space: 45.2 MB (5.82%) - LeetHub

--- 110-balanced-binary-tree/110-balanced-binary-tree.java
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -50,5 +50,9 @@ class Solution {
 
 //every function call tells both if a particular subtree is balanced or not and it also returns the height...
 // if unbalanced..function returns -1...else it returns height which can be used to check the balance of other level(nodes) of tree
+// the else part is the function to get height..how we used to get max height of binary tree max(lh,rh)+1
+// nd that lh or rh = -1 condition will be triggered when lh-rh will be greater than 1 for some node as then function will return -1 acc to code
+// and hence for its parent like for whose node lh-rh was >1 when the recursive call will backtrack then control will get back to parent 
+// and parent will get either lh or rh = -1 as -1 was returned in previous call ...so in that condition also return -1 as tree unbalanced
 // this function returns -1 if a subtree is unbalanced else it returns the height of that subtree
 // if ever -1 is returned then immediately return false as even if one unbalanced subtree the whole tree is unbalanced..else return true in bool isBalanced function
