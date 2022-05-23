904c83b5012f129da885b6406929d8c2daa909a3
Achintya Kumar Satyayan <67166146+aksatyayan-star@users.noreply.github.com>
2022-05-23 15:54:18 +0530

    Time: 1 ms (94.82%), Space: 44.6 MB (28.28%) - LeetHub

---
+++ 110-balanced-binary-tree/110-balanced-binary-tree.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+       if(heightBalanced(root) == -1)
+       {
+           return false;
+       }
+       return true;
+    }
+    int heightBalanced(TreeNode root)
+    {
+       if(root==null)
+        {
+            return 0;
+        }
+        int lh = heightBalanced(root.left);
+        if(lh == -1)
+        {
+            return -1;
+        }
+        int rh = heightBalanced(root.right);
+        if(rh == -1)
+        {
+            return -1;
+        }
+        if(Math.abs(lh-rh)>1)
+        {
+            return -1;
+        }
+        else
+        {
+            return Math.max(lh,rh)+1;
+        } 
+    }
+}
+
+//every function call tells both if a particular subtree is balanced or not and it also returns the height...
+// if unbalanced..function returns -1...else it returns height which can be used to check the balance of other level of tree
+// this function returns -1 if a subtree is unbalanced else it returns the height of that subtree
+// if ever -1 is returned then immediately return false as even if one unbalanced subtree the whole tree is unbalanced..else return true in bool isBalanced function
