a78576f4bf1b177e184a53a81bb99820f1e6ba97
Achintya Kumar Satyayan <67166146+aksatyayan-star@users.noreply.github.com>
2022-05-26 00:09:37 +0530

    Added solution - LeetHub

---
+++ Left View of Binary Tree - GFG/left-view-of-binary-tree.java
@@ -0,0 +1,156 @@
+// { Driver Code Starts
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GfG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root)
+    {
+        if(root == null)
+            return;
+            
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        
+        printInorder(root.right);
+    }
+    
+	public static void main (String[] args) throws IOException{
+	        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	        
+	        int t=Integer.parseInt(br.readLine());
+    
+	        while(t > 0){
+	            String s = br.readLine();
+    	    	Node root = buildTree(s);
+        	    Tree g = new Tree();
+    			ArrayList<Integer> result = g.leftView(root);
+    			for(int value : result){
+    			    System.out.print(value + " ");
+    			}
+    			System.out.println();
+                t--;
+        }
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/* A Binary Tree node
+class Node
+{
+    int data;
+    Node left, right;
+
+    Node(int item)
+    {
+        data = item;
+        left = right = null;
+    }
+}*/
+class Tree
+{
+    //Function to return list containing elements of left view of binary tree.
+    ArrayList<Integer> ans = new ArrayList<Integer>();
+    ArrayList<Integer> leftView(Node root)
+    {
+        return left(root,1);
+    }
+    int maxlevel=0;
+    ArrayList<Integer> left(Node root, int level)
+    {
+      // Your code here
+      if(root==null)
+      {
+          return ans;
+      }
+      if(maxlevel<level)
+      {
+          ans.add(root.data);
+          maxlevel=level;
+      }
+      left(root.left, level+1);
+      left(root.right, level+1);
+      return ans;
+    }
+}
+
+// the concept is similar to     199. Binary Tree Right Side View
+// but there i did with a diff approach using level order traversal
+// here im doing it with diff recursive approach...that level order approach can also be applied here
+// the trick is that if we do preorder traversal of a binary tree then we will always visit the leftmost node first before any other node of a level
+// so we do preorder traversal...while doing preorder traversal..we maintain two var maxlevel and level
+// maxlevel for what we have visited so far and level is for current level which we currently on i.e the node's level
+// initially maxlevel = 0 and level =1 and whenever maxlevel < level we add that element to array and make maxlevel to level
+// and again recursively call for left and right while incrementing level by 1
