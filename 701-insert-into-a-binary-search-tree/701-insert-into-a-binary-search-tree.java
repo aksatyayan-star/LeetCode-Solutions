b8ecbf99553f5090e61053a778da5daeb5fb7fa8
Achintya Kumar Satyayan <67166146+aksatyayan-star@users.noreply.github.com>
2022-05-26 18:09:19 +0530

    Time: 0 ms (100.00%), Space: 54.9 MB (13.77%) - LeetHub

--- 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
+++ 701-insert-into-a-binary-search-tree/701-insert-into-a-binary-search-tree.java
@@ -49,3 +49,21 @@ class Solution {
         return curr;
     }
 }
+
+// so the concept is pretty simple...we have to insert item following the conditions of BST i.e if element greater than root insert in right if smaller insert in left
+//and if same then do nothing but as written there are no repetetive elements but still if present then it will not fall in any if conditions 
+// and so will simply return curr which is written at the last of code...curr which is the root of the whole tree
+// so first what we need to do is store the root in curr var...as in the end after doing all operations we need to return the root of whole tree
+// so as its recursive so we cant store root in curr without any condition inside function as then for each recursive call the value of curr will keep updating
+// hence we defined curr and one another var level outside function in the class...and intialized both as 0...
+// so what we do intially for the first call level is 0 so we check in if condition if level 0 then store root in curr...this way
+// in the 1st call obviously root will be the topmost root node of tree and that got stored in curr...and now for each recursive calls level
+// would be incremented as level++ done in each else if condition..so level wont be 0 again hence no updation of curr will happen..the root will be stored there safely
+// now if root == null i.e empty tree in that condition whatever value has to be inserted will be the only node and hence the root node of the tree...so return ins
+// ins is the node we created for the given value to be inserted
+// else if root.val > val we will go in the left side but here also one extra condition what if that is the leaf node..so we check for root.left=null
+// if yes then we insert our node in the left of this node..and after inserting also we can simply return curr there
+// or we can leave and let the recursive function call outside if condn for left side happen..in that condition what will happen the function will be called for a node(root.left)
+// whose value is equal to val..i.e the val we had to insert as this is that inserted node only which we just inserted in if condition
+// so it will not fall under any if else conditions and hence go the last and will return curr..ultimately the same thing which we wanted to return
+// same in the case for right calls...thats all..
