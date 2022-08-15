1f7b4e4d19cb274bff2d01e9d0deed476d94e941
Achintya Kumar Satyayan <67166146+aksatyayan-star@users.noreply.github.com>
2022-08-15 19:47:59 +0530

    Time: 2 ms (27.06%), Space: 43.8 MB (40.37%) - LeetHub

--- 31-next-permutation/31-next-permutation.java
+++ 31-next-permutation/31-next-permutation.java
@@ -57,4 +57,8 @@ class Solution {
 }
 
 // the solution is tricky little bit what we doing is that we find a element from end side of array if we encounter an element greater than its previous element from the end side
-// then we swap those 
+// then we swap that previous element i.e i-1 brk point element with again the element we again search from end till this element that we got and the first element that we encounter greater than this 
+// we store its index in next...nd we call swap function for these two indices brk and next...and do the req swapping...once that swapping done
+// along with that we also maintain a bool var...which we set true if there is some element greater than its previous element from the end side
+// else bool false...so if bool true and after swapping done we will sort rest elements of array from one element next to brk point i.e brk+1 till last of array
+// else if bool false...then just simply sort the whole array as that means there were no elements greater than its previous element from the end side of array...nums = [3,2,1]...this case
